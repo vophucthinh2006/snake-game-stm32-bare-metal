4715a17c2f30b045999caa672afb5b0eb4185ded
vophucthinh2006 <thinhvo2006vn@gmail.com>
2026-02-10 01:48:38 +0700

    Add files via upload

---
+++ logic/source/FOUR_BIT_LED.c
@@ -0,0 +1,127 @@
+#include "FOUR_BIT_LED.h"
+
+static volatile uint32_t *LED_BSRR;
+static volatile uint32_t *LED_BRR;
+
+static uint8_t LATCH_PIN;
+static uint8_t CLOCK_PIN;
+static uint8_t DATA_PIN;
+
+static const uint8_t SEG_CODE[17] =
+{
+	0b11000000, //0
+	0b11111001, //1
+	0b10100100, //2
+	0b10110000, //3
+	0b10011001, //4
+	0b10010010, //5
+	0b10000010, //6
+	0b11111000, //7
+	0b10000000, //8
+	0b10010000, //9
+	0b10001100, //A
+	0b10111111, //B
+	0b11000110, //C
+	0b10100001, //D
+	0b10000110, //E
+	0b11111111, //F
+    0xBF        // -
+};
+
+/*
+		BIT_0
+	BIT_5	BIT_1
+		BIT6
+	BIT_4	BIT_2
+		BIT_3		BIT_7
+*/
+
+void FOUR_BIT_LED_Config(
+		GPIO_PORT_t port,
+        uint8_t latch_pin,
+        uint8_t clock_pin,
+        uint8_t data_pin
+		){
+    switch(port){
+        case GPIO_PORTA:
+            LED_BSRR = &GPIOA_BSRR;
+            LED_BRR  = &GPIOA_BRR;
+            break;
+
+        case GPIO_PORTB:
+            LED_BSRR = &GPIOB_BSRR;
+            LED_BRR  = &GPIOB_BRR;
+            break;
+
+        case GPIO_PORTC:
+            LED_BSRR = &GPIOC_BSRR;
+            LED_BRR  = &GPIOC_BRR;
+            break;
+
+        default:
+            return;
+    }
+
+    LATCH_PIN = latch_pin;
+    CLOCK_PIN = clock_pin;
+    DATA_PIN  = data_pin;
+}
+
+void FOUR_BIT_LED_Send16(uint16_t data){
+    *LED_BRR = (1 << LATCH_PIN);
+
+    for(int i = 15; i >= 0; i--)
+    {
+        if(data & (1 << i)) *LED_BSRR = (1 << DATA_PIN);
+        else                *LED_BRR  = (1 << DATA_PIN);
+
+        //for(volatile int i = 0; i < 1; i++);
+        *LED_BRR  = (1 << CLOCK_PIN);
+        //or(volatile int i = 0; i < 1; i++);
+        *LED_BSRR = (1 << CLOCK_PIN);
+    }
+    //for(volatile int i = 0; i < 1; i++);
+    *LED_BSRR = (1 << LATCH_PIN);
+}
+
+void FOUR_BIT_LED_Display(
+		uint8_t digit1,
+		uint8_t digit2,
+		uint8_t digit3,
+		uint8_t digit4
+		){
+    uint16_t data;
+
+    data = ((uint16_t)SEG_CODE[digit1] << 8) | 0x08;
+    FOUR_BIT_LED_Send16(data);
+
+    data = ((uint16_t)SEG_CODE[digit2] << 8) | 0x04;
+    FOUR_BIT_LED_Send16(data);
+
+    data = ((uint16_t)SEG_CODE[digit3] << 8) | 0x02;
+    FOUR_BIT_LED_Send16(data);
+
+    data = ((uint16_t)SEG_CODE[digit4] << 8) | 0x01;
+    FOUR_BIT_LED_Send16(data);
+}
+
+void FOUR_BIT_LED_Scan(
+		uint8_t digit1,
+		uint8_t digit2,
+		uint8_t digit3,
+		uint8_t digit4
+		){
+	static uint8_t digit = 1;
+
+	switch(digit){
+	case 1: FOUR_BIT_LED_Send16(((uint16_t)SEG_CODE[digit1] << 8 | 0x08)); break;
+	case 2: FOUR_BIT_LED_Send16(((uint16_t)SEG_CODE[digit2] << 8 | 0x04)); break;
+	case 3: FOUR_BIT_LED_Send16(((uint16_t)SEG_CODE[digit3] << 8 | 0x02)); break;
+	case 4: FOUR_BIT_LED_Send16(((uint16_t)SEG_CODE[digit4] << 8 | 0x01)); break;
+	default: return;
+	}
+	if (digit < 4){
+		digit++;
+	}
+	else digit = 1;
+}
---
+++ logic/source/OLED.c
@@ -0,0 +1,103 @@
+#include "OLED.h"
+
+uint8_t OLED_BYTE[oled_x * (oled_y / 8)];
+
+void OLED_Init(void){
+	uint8_t oled_init_cmd[32];
+	uint8_t cmd_length = 0;
+
+	oled_init_cmd[cmd_length++] = Display_On_Off(LOW);
+	//oled_init_cmd[cmd_length++] = Set_Page_Address(0);
+	oled_init_cmd[cmd_length++] = Contrast_Control_Register_On();
+	oled_init_cmd[cmd_length++] = Set_Contrast_Control_Register(0xFF);
+	oled_init_cmd[cmd_length++] = Set_Segment_Re_Map(HIGH);
+	oled_init_cmd[cmd_length++] = Set_Normal_Reverse_Display(LOW);
+	oled_init_cmd[cmd_length++] = Multiplex_Ration_On();
+	oled_init_cmd[cmd_length++] = Set_Multiplex_Ration(64);
+	oled_init_cmd[cmd_length++] = DC_DC_Control_On();
+	oled_init_cmd[cmd_length++] = Set_DC_DC_Control(HIGH);
+	oled_init_cmd[cmd_length++] = Set_Pump_Voltage_Value(EIGHT_POINT_ZERO);
+	oled_init_cmd[cmd_length++] = Set_Common_Output_Scan_Direction(HIGH);
+	oled_init_cmd[cmd_length++] = Display_Offset_On();
+	oled_init_cmd[cmd_length++] = Set_Display_Offset(0x00);
+	oled_init_cmd[cmd_length++] = Display_Clock_Divide_Ratio_On();
+	oled_init_cmd[cmd_length++] = Set_Display_Clock_Divide_Ratio(8, SH1106_FREQUENCY_MINUS_25_PERCENT);
+	oled_init_cmd[cmd_length++] = Dis_Charge_Pre_Charge_Period_On();
+	oled_init_cmd[cmd_length++] = Set_Dis_Charge_Pre_Charge_Period(1, 0xF);
+	oled_init_cmd[cmd_length++] = Common_Pads_Hardware_Configuration_On();
+	oled_init_cmd[cmd_length++] = Set_Common_Pads_Hardware_Configuration(HIGH);
+	oled_init_cmd[cmd_length++] = VCOM_Deselect_Level_On();
+	oled_init_cmd[cmd_length++] = Set_VCOM_Deselect_Level(64);
+	oled_init_cmd[cmd_length++] = Set_Display_Start_Line(0);
+	oled_init_cmd[cmd_length++] = Display_On_Off(HIGH);
+
+	SH1106_I2C_Transmit(SH1106_TYPE_cmd, oled_init_cmd, cmd_length);
+};
+
+void OLED_Set_Pixel(uint8_t x, uint8_t y, OLED_COLOR_t color){
+	if ((x >= oled_x) || (y >= oled_y)){
+		return;
+	}
+	uint8_t real_x = oled_x - 1 - x;
+	uint8_t real_y = oled_y - 1 - y;
+	uint16_t byte = real_x + ((real_y/8) * oled_x);
+
+	if (color == WHITE){
+		OLED_BYTE[byte] |= (1 << (real_y % 8));
+	}
+	else{
+		OLED_BYTE[byte] &= ~(1 << (real_y % 8));
+	}
+}
+
+void OLED_Set_All(void){
+	for (int byte = 0; byte < (8 * oled_x); byte++){
+		OLED_BYTE[byte] = 0xFF;
+	}
+}
+
+void OLED_Clear_All(void){
+	for (int byte = 0; byte < (8 * oled_x); byte++){
+		OLED_BYTE[byte] = 0x00;
+	}
+}
+
+void OLED_Update_Screen(void){
+	uint8_t oled_update[4];
+
+	for (uint8_t page = 0; page < 8; page++){
+		uint8_t update_length = 0;
+
+		oled_update[update_length++] = Set_Page_Address(page);
+		oled_update[update_length++] = Set_Lower_Column_Address(0b0010);
+		oled_update[update_length++] = Set_Higher_Column_Address(0b0000);
+
+
+		SH1106_I2C_Transmit(SH1106_TYPE_cmd, oled_update, 3);
+		SH1106_I2C_Transmit(SH1106_TYPE_data, &OLED_BYTE[page * oled_x], oled_x);
+	}
+}
+void OLED_Update_Page(uint8_t page){
+	uint8_t oled_update[4];
+	uint8_t update_length = 0;
+
+	oled_update[update_length++] = Set_Page_Address(page);
+	oled_update[update_length++] = Set_Lower_Column_Address(0b0010);
+	oled_update[update_length++] = Set_Higher_Column_Address(0b0000);
+
+	SH1106_I2C_Transmit(SH1106_TYPE_cmd, oled_update, 3);
+	SH1106_I2C_Transmit(SH1106_TYPE_data, &OLED_BYTE[page * oled_x], oled_x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
